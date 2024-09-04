db2e8a2025ea844fa8f5d1de1b3199ea84f588ec
Jerome Bell <jeromebell@gmail.com>
2024-09-04 14:48:22 -0500

    Try Fix 1

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -8,10 +8,10 @@ window.onload = function() {
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
-      EditorPreviewAsyncAPIPlugin,
       SwaggerUIStandalonePreset
     ],
     plugins: [
+      SwaggerEditor.plugins.EditorPreviewAsyncAPI,
       SwaggerUIBundle.plugins.DownloadUrl
     ],
     layout: "StandaloneLayout"
